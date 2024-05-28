bc11fbe9dd0700a3d489d6fd66c4203d1ec320b6
FlipAlexCosta <fafc04@gmail.com>
2024-05-28 17:58:08 +0100

    Cenas

--- aux/OLAP.sql
+++ aux/OLAP.sql
@@ -1,41 +1,31 @@
+-- 1
 %%sql
--- SELECT ...
-SELECT DISTINCT
-    res.ssn
-FROM
-    (SELECT h.ssn, (MAX(h.data) - MIN(h.data)) AS intervalo
-    FROM historial_paciente h INNER JOIN consulta c USING(id)
-    WHERE h.especialidade = 'ortopedia' AND h.tipo = 'observacao' AND h.valor IS NULL
-    GROUP BY h.ssn, h.chave HAVING (MAX(h.data) - MIN(h.data)) > INTERVAL '0') AS res
-WHERE res.intervalo >= ALL (
-    SELECT (MAX(h.data) - MIN(h.data))
-    FROM historial_paciente h INNER JOIN consulta c USING(id)
-    WHERE h.especialidade = 'ortopedia' AND h.tipo = 'observacao' AND h.valor IS NULL
-    GROUP BY h.ssn, h.chave HAVING (MAX(h.data) - MIN(h.data)) > INTERVAL '0'
+REFRESH MATERIALIZED VIEW historial_paciente;
+SELECT DISTINCT ssn
+FROM historial_paciente
+WHERE especialidade = 'ortopedia' AND tipo = 'observacao' AND valor IS NULL
+GROUP BY ssn, chave
+HAVING MAX(data) - MIN(data) >= ALL (
+    SELECT MAX(data) - MIN(data)
+    FROM historial_paciente
+    WHERE especialidade = 'ortopedia' AND tipo = 'observacao' AND valor IS NULL
+    GROUP BY ssn, chave
 );
 
-Determinar que medicamentos estão a ser usados para tratar doenças crónicas do foro
-cardiológico. Considera-se que qualificam quaisquer medicamentos receitados ao mesmo
-paciente (qualquer que ele seja) pelo menos uma vez por mês durante os últimos doze meses, em consultas de cardiologia
-
+-- 2
 %%sql
--- SELECT ...
-SELECT DISTINCT
-    chave AS nome_medicamento
-FROM
-    historial_paciente
-WHERE
-    h1.tipo = 'receita' AND h1.especialidade = 'cardiologia'
-    AND (12 * (EXTRACT(YEAR FROM CURRENT_DATE()) - ano) + 
-      (EXTRACT(MONTH FROM CURRENT_DATE()) - mes)) BETWEEN 0 AND 11
-GROUP BY
-    ssn, chave
-HAVING
-    COUNT(data) >= 12
-;
+REFRESH MATERIALIZED VIEW historial_paciente;
+SELECT DISTINCT chave AS medicamento
+FROM historial_paciente
+WHERE especialidade = 'cardiologia' AND tipo = 'receita' AND
+    (12 * (EXTRACT(YEAR FROM NOW()::date) - ano) + (EXTRACT(MONTH FROM NOW()::date) - mes)) BETWEEN 0 AND 11
+GROUP BY ssn, chave
+HAVING COUNT(data) >= 12;
 
+
+-- 3
 %%sql
--- SELECT ...
+REFRESH MATERIALIZED VIEW historial_paciente;
 SELECT
     SUBSTRING(localidade FROM '[0-9]{4}-[0-9]{3} (.+)$') AS localidade, h.nome, mes, dia_do_mes, h.especialidade, m.nome AS nome_medico, SUM(valor) AS quantidade_total
 FROM
